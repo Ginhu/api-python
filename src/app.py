d8f64ee3c83f5b3dc7ce0997fbffeae1ac21ee98
Flávio Fonseca <ff@darkpenguin.com.br>
2024-04-19 13:52:19 -0300

    Adicona libs (#2)
    * flake 8
    
    * ad. submodule libs

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+from src.libs import dh
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return JSONResponse({'status': 'online'})
+
+
+@app.get('/agora')
+def agora():
+    return JSONResponse({'agora': dh.hoje_zero_hora_iso()})
--- testes/api/teste_app.py
+++ testes/api/teste_app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi.testclient import TestClient
 import pytest
 
@@ -16,3 +17,12 @@ def testa_decodifica_token(api: TestClient):
     ret = api.get(url)
     assert ret.status_code == 200
     assert ret.json() == {'status': 'online'}
+
+
+def testa_libs_acessivel(api: TestClient):
+    url = '/agora'
+    ret = api.get(url)
+    assert ret.status_code == 200
+    ret_json = ret.json()
+    agora = datetime.now()
+    assert agora.date().isoformat() in ret_json['agora']
